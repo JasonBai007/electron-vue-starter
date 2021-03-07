6567171cb1e1009d6b515e78f3bd61a627cb89d2
JasonBai007 <byygkcg@163.com>
2021-03-07 17:20:34 +0800

    集成echarts

---
+++ src/views/Chart.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="about">
+    <h1>This is a chart page</h1>
+    <div id="container"></div>
+  </div>
+</template>
+
+<script>
+import * as echarts from "echarts";
+export default {
+  name: "Chart",
+  myChart: null,
+  data() {
+    return {};
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      this.myChart = echarts.init(document.getElementById("container"));
+      // 绘制图表
+      this.myChart.setOption({
+        title: {
+          text: "ECharts 入门示例",
+        },
+        tooltip: {},
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "销量",
+            type: "bar",
+            data: [5, 20, 36, 10, 10, 20],
+          },
+        ],
+      });
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+#container {
+  width: 300px;
+  height: 200px;
+}
+</style>
