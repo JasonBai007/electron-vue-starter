e5702dac41057f8906adb2c27b67c219d23e7126
JasonBai007 <byygkcg@163.com>
2021-03-07 17:39:31 +0800

    完善关于我们页面

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,6 +3,7 @@
     <h1>This is an about page</h1>
     <p>Press Ctrl+F12 to open Dev tools to check Network.</p>
     <el-button type="primary" @click="getData" size="small">Get XHR Data</el-button>
+    <pre>{{resData}}</pre>
   </div>
 </template>
 
@@ -10,15 +11,25 @@
 export default {
   name: "About",
   data() {
-    return {};
+    return {
+      resData: "",
+    };
   },
   mounted() {},
   methods: {
     getData() {
       this.$http.get("passport").then((res) => {
-        console.log(res);
+        this.resData = JSON.stringify(res, null, 4);
       });
     },
   },
 };
 </script>
+<style lang="scss" scoped>
+pre {
+  text-align: left;
+  width: 200px;
+  margin: 30px auto;
+  font-weight: bold;
+}
+</style>
